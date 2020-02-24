1a550df63676b163f385af54d25fe9c637dabbf0
Jeff Labonte <grimsleepless@protonmail.com>
2020-02-23 23:57:31 -0500

    Test a few functions and code them myself

--- dive_in/baby.hs
+++ dive_in/baby.hs
@@ -4,3 +4,16 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
+
+nouns = ["dude","gall","partner"]
+adjectives = ["nice","honorable","dishonorable"]
+buildStrings = [adjective ++ " " ++ noun| adjective <- adjectives, noun <- nouns]
+
+length' xs = sum [1 |_<-xs]
+
+{-
+It requires a list of list with numbers
+e.g: [ [0,2,4,5,6,7,8,10], [2,4,6,7,8,9] ]
+-}
+retriveveEvenNumberFromList xss = [ [x | x <- xs, even x ] | xs <- xss]
+
