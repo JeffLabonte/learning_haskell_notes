f9a781e41bfcb1f2f4c0acad8ee72cd1b9f4f549
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-09 23:25:24 -0400

    Equivalent to Enums

---
+++ Real_World_Haskell_book/ch03/enum.hs
@@ -0,0 +1,10 @@
+-- file: ch03/enum.hs
+
+data Roygbic = Red
+             | Orange
+             | Yellow
+             | Green
+             | Blue
+             | Indigo
+             | Violet
+             deriving (Eq, Show)
