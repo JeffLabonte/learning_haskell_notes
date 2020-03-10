ff73e1c4e8e5ef4369598d2cc6d2896bb5184f12
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-09 22:09:57 -0400

    Update signature and create a function for exercise

--- Real_World_Haskell_book/ch02/myDrop.hs
+++ Real_World_Haskell_book/ch02/myDrop.hs
@@ -1,5 +1,10 @@
 -- file: ch02/myDrop.hs
 
+myDrop :: Int -> [a] -> [a]
 myDrop n xs = if n <= 0 || null xs
               then xs
               else myDrop (n - 1) (tail xs)
+
+-- exercise Chapter 02
+lastButOne :: [a] -> a
+lastButOne xs = xs !! ((length xs) - 2) -- Running with too short array, errors negative index
