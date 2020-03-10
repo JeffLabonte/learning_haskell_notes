710e152ad04afa01330f88a27735d17d8d51c65c
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-09 22:44:49 -0400

    Add constructor and type synonyms

--- Real_World_Haskell_book/ch03/Bookstore.hs
+++ Real_World_Haskell_book/ch03/Bookstore.hs
@@ -1,9 +1,18 @@
 -- file: ch03/Bookstore.hs
 
+type CustomerID = Int
+type ReviewBody = String
+
 data BookInfo = Book Int String [String]
                deriving (Show)
 
 data MagazineInfo = Magazine Int String [String]
                     deriving (Show)
+
+
+data BookReview = BookReview BookInfo CustomerID ReviewBody  -- Better Review
+
+type BookRecord = (BookInfo, BookReview)
+
 
 myInfo = Book 0 "Algebra of Programming" ["Richard Bird", "Oege de Moor"]
