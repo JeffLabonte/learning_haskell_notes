e37852d45c28c6ce9218ca9545f1a32904226670
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-29 18:15:23 -0400

    Fix issue of the BadPattern example

--- Real_World_Haskell_book/ch03/BadPattern.hs
+++ Real_World_Haskell_book/ch03/BadPattern.hs
@@ -1,7 +1,7 @@
 -- ch03/BadPattern.hs
 {-
-    badExample (x:xs) = x + badExample + xs
+    badExample (x:xs) = x + badExample xs
 -}
 
-goodExample (x: xs) = x + goodExample + xs
+goodExample (x: xs) = x + goodExample xs
 goodExample _       = 0
