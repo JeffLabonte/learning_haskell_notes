daf81d360e8bd7cb0bdb5e82729da08cbb846cf9
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-30 19:12:49 -0400

    Update repo

---
+++ Real_World_Haskell_book/cha03/ListADT.hs
@@ -0,0 +1,9 @@
+-- file: ch03/ListADT.hs
+
+data List a = Cons a (List a)
+            | Nil
+              deriving (Show)
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
---
+++ Real_World_Haskell_book/cha03/Tree.hs
@@ -0,0 +1,2 @@
+-- file: ch03/Tree.hs
+simpleTree = Node "parent" (Node "left child" Empty Empty) (Node "right child" Empty Empty)
