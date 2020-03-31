f01ef28b03c5c53ed4184e91a31a03b705102cfb
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-30 20:36:31 -0400

    Update MySecond and create Lending

--- Real_World_Haskell_book/ch03/MySecond.hs
+++ Real_World_Haskell_book/ch03/MySecond.hs
@@ -5,6 +5,13 @@ mySecond xs = if null (tail xs)
               then error "list too short"
               else head (tail xs)
 
+safeSecond :: [a] -> Maybe a
+
 safeSecond xs = if null (tail xs)
                 then Nothing
                 else Just (head (tail xs))
+
+tidySecond :: [a] -> Maybe a
+
+tidySecond (_:x:_) = Just x
+tidySecond _       = Nothing
