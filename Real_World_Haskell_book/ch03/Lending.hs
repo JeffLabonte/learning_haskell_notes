d18d85bba287b62c3af8b65e2de10365a81e4dcb
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-31 16:20:23 -0400

    Add documentation top code

--- Real_World_Haskell_book/ch03/Lending.hs
+++ Real_World_Haskell_book/ch03/Lending.hs
@@ -1,12 +1,13 @@
 -- file: ch03/Lending.hs
 
+lend :: (Ord a, Fractional a) => a -> a -> Maybe a
 lend amount balance = let reserve = 100
                           newBalance = balance - amount
                       in if balance < reserve
                           then Nothing
                           else Just newBalance
 
-
+lend2 :: (Ord a, Fractional a) => a -> a -> Maybe a
 lend2 amount balance = if amount < reserve * 0.5
                        then Just newBalance
                        else Nothing
