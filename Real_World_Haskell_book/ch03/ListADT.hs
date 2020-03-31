03507f3d3031fe42d42dd8dbfefa12639f74e530
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-30 20:12:59 -0400

    Update code

--- Real_World_Haskell_book/ch03/ListADT.hs
+++ Real_World_Haskell_book/ch03/ListADT.hs
@@ -7,3 +7,10 @@ data List a = Cons a (List a)
 data Tree a = Node a (Tree a) (Tree a)
             | Empty
               deriving (Show)
+
+
+{-
+  Excercise From List
+-}
+
+fromList (x:xs) = Cons x (fromList xs)
