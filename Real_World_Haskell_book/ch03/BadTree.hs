43f5230a866322f17af456b658c6b5dc2976554f
Jeff Labonte <jeff.labonte12@gmail.com>
2020-03-31 22:52:34 -0400

    Add constructor for Node, Add document

--- Real_World_Haskell_book/ch03/BadTree.hs
+++ Real_World_Haskell_book/ch03/BadTree.hs
@@ -5,7 +5,9 @@ bad_nodesAreSame (Node a _ _) (Node a _ _) = Just a
 
 bad_nodesAreSame _              _           = Nothing
 -}
+data Node = Node Int Int Int
 
+nodesAreSame :: Node -> Node -> Maybe Int
 nodesAreSame (Node a _ _) (Node b _ _)
     | a == b      = Just a
 nodesAreSame _  _ = Nothing
